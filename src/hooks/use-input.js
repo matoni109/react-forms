e76f8c081db8fab2909e134212d39a319de0d0f5
Chris Wade <chris@wadespace.net>
2021-05-08 11:21:19 +1000

    update

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useInput = (validateValue) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isTouched, setIsTouched] = useState(false);
-
+  // value is valid comes from input validator
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
 
